7fb0eab44ce97fdaa1454e8906e84d824872a270
welitonap <55259783+welitonap@users.noreply.github.com>
2020-03-06 11:59:36 -0300

    Insert Files

---
+++ body.php
@@ -0,0 +1,16 @@
+<div id="menu">
+
+  HOME
+
+  <div id="userImg">
+    <div id="statuId">
+
+    </div>
+    <div id="subMenu">
+      <span>Perfil</span>
+      <span>Option</span>
+      <span>Exit</span>
+    </div>
+
+  </div>
+</div>
